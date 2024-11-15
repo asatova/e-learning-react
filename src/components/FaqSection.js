05dcd4e5d760149e9f1d939fa60f71aa90581199
zarnigor <zarnigorasatova52@gmail.com>
2024-11-15 12:14:05 +0500

    intro styled componentes

---
+++ src/components/FaqSection.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FaqSection = () => {
+    return
+     <div className="faq">
+        <h2>Any Questions? <span>FAQ</span></h2>
+        <div className="question">Can i learning from 0? </div>
+        <div className="answer">lorem</div>
+        <div className="question">how much i have to pay? </div>
+        <div className="answer">lorem</div>
+        <div className="question">Can i learning from 0 </div>
+        <div className="answer">lorem</div>
+   
+    </div>
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,11 +2,12 @@ import React from "react";
 //import components
 import AboutSection from "../components/AboutSection";
 import BenefitSection from "../components/Benefits";
+import FaqSection from "../components/FaqSection";
 const AboutUs = () => {
     return <div>
         <AboutSection />
         <BenefitSection />
-
+        <FaqSection />
     </div>;
 };
 
