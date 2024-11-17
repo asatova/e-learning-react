c19ae9c11270a117791eeea922b2f158df18934e
zarnigor <zarnigorasatova52@gmail.com>
2024-11-17 18:15:45 +0500

    faq section styling completed

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,18 +1,43 @@
+import { color } from "framer-motion";
 import React from "react";
-
+import  styled from "styled-components";
+import {About} from "../styles";
 const FaqSection = () => {
     return(
-     <div className="faq">
+     <Faq>
         <h2>Any Questions? <span>FAQ</span></h2>
-        <div className="question">Can i learning from 0? </div>
-        <div className="answer">lorem</div>
-        <div className="question">how much i have to pay? </div>
-        <div className="answer">lorem</div>
-        <div className="question">Can i learning from 0 </div>
-        <div className="answer">lorem</div>
-   
-    </div>
+        <div className="question"><h4>Can i learning from 0? </h4>
+        <div className="answer"><p>lorem</p></div><div className="faq-line"></div></div>
+        <div className="question"><h4>Can i learning from 0? </h4>
+        <div className="answer"><p>lorem</p></div><div className="faq-line"></div></div>
+        <div className="question"><h4>Can i learning from 0? </h4>
+        <div className="answer" ><p>lorem</p></div></div>
+        
+    </Faq>
     );
 };
+
+const Faq = styled(About) `
+display: block;
+h2{
+    padding-bottom: 2rem;
+    font-weight: lighter;
+}
+.question{
+    padding:3rem 0rem;
+    cursor: pointer;
+};
+.answer{
+    padding:2rem 3rem;
+
+}
+.faq-line{
+    background-color: #ccc;
+    height: 0.2rem;
+    width: 100%;
+    margin: 1rem 0rem;
+
+}
+`
 
 export default FaqSection;
